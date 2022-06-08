9bf6b9538ccf061f6277af58cec45cc09d56dbbd
boon chi <hum.boon.chi@gmail.com>
2022-06-08 10:20:46 +0200

    ✨ add eslint,prettier and pre-commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   verbose: true,
-  // automock: true,
-};
-export default config;
+  coveragePathIgnorePatterns: ['./dist/', './node_modules/', './coverage/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+}
+export default config
