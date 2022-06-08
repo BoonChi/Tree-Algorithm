c88d1a55673771a31e0d2ff2ac9b2aa909d06761
boon chi <hum.boon.chi@gmail.com>
2022-06-08 11:43:42 +0200

    ✨ remove node feature with tests 🐛 remove undefined checking 🐛 replace setDirection with getDirection ✅ add more use cases on removing specific node to make sure no other node is being removed ⚡️ remove spread object to optimize performance

--- index.ts
+++ index.ts
@@ -1,25 +1,84 @@
 type ITreeNode<TValue> = {
-  left?: ITreeNode<TValue>
-  right?: ITreeNode<TValue>
-  value: TValue
-}
+  left?: ITreeNode<TValue>;
+  right?: ITreeNode<TValue>;
+  value: TValue;
+};
 
 export class Tree<TValue extends number> {
-  public root?: ITreeNode<TValue>
+  public root?: ITreeNode<TValue>;
 
   public insert(value: TValue) {
     if (this.root) {
-      this._insert(this.root, value)
+      this._insert(this.root, value);
     } else {
       this.root = {
         value,
-      }
+      };
     }
   }
 
   private _insert(node: ITreeNode<TValue>, value: TValue) {
-    const direction = value >= node.value ? 'right' : 'left'
-    if (node[direction] !== undefined) this._insert(node[direction]!, value) // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    else node[direction] = { value }
+    const direction = this._getDirection(node, value);
+    const nextNode = node[direction];
+    if (nextNode) this._insert(nextNode, value);
+    else node[direction] = { value };
+  }
+
+  private _getDirection(node: ITreeNode<TValue>, value: TValue) {
+    return value >= node.value ? 'right' : 'left';
+  }
+
+  public remove(value: TValue): boolean {
+    if (!this.root) return false;
+    if (value === this.root.value) {
+      this.root = undefined;
+      return true;
+    } else {
+      return this._remove(this.root, value);
+    }
+  }
+
+  private _remove(node: ITreeNode<TValue>, value: TValue): boolean {
+    const direction = this._getDirection(node, value);
+    const nextNode = node[direction];
+
+    if (!nextNode) {
+      return false;
+    }
+
+    if (nextNode.value !== value) return this._remove(nextNode, value);
+
+    if (nextNode.left && nextNode.right && nextNode.left.right) {
+      node[direction] = {
+        value: nextNode.left.value,
+        left: nextNode.left.left,
+        right: {
+          value: nextNode.left.right.value,
+          right: {
+            value: nextNode.right.value,
+            left: nextNode.right.left,
+            right: nextNode.right.right,
+          },
+        },
+      };
+      return true;
+    } else if (nextNode.left) {
+      node[direction] = {
+        value: nextNode.left.value,
+        left: nextNode.left.left,
+        right: nextNode.left.right,
+      };
+      return true;
+    } else if (nextNode.right) {
+      node[direction] = {
+        value: nextNode.right.value,
+        left: nextNode.right.left,
+        right: nextNode.right.right,
+      };
+      return true;
+    } else {
+      delete node[direction];
+      return true;
+    }
   }
 }
